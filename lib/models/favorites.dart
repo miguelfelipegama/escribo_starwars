9b6f2f0cf3d92992350243fe9cf76794f997ea74
Miguel Felipe da Gama Melo <fiscaltodahora@gmail.com>
2022-09-08 17:09:31 -0300

    Modifica servico SQLite

--- lib/models/favorites.dart
+++ lib/models/favorites.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:testing_app/servicos/constants_exceptions.dart';
 
 /// The [Favorites] class holds a list of favorite items saved by the user.
 class Favorites extends ChangeNotifier {
@@ -24,6 +25,12 @@ class Favorites extends ChangeNotifier {
 class Favorite {
   final String texto;
   final bool origem;
-
+  Favorite.fromRow(Map<String, Object?> map)
+      : texto = map[favTextColumn] as String,
+        origem = (map[favTypeColumn] as int) == 1 ? true : false;
   Favorite({required this.texto, required this.origem});
+  @override
+  bool operator ==(covariant Favorite other) => texto == other.texto;
+  @override
+  int get hashCode => texto.hashCode;
 }
